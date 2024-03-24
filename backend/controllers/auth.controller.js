ed644d515437aabb48793f78b07919ed944b23b2
Anushk Jain <anushk2201@gmail.com>
2024-03-24 15:02:24 +0530

    done: catchAsync to catch errors from any async function.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,49 +1,47 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import AppError from "../utils/appError.js";
+import { catchAsync } from "../utils/catchAsync.js";
 
-export const signup = async (req, res, next) => {
-    try {
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+export const signup = catchAsync(async (req, res, next) => {
 
-        if (password !== confirmPassword) {
-            return next(new AppError("Passwords Do Not Match", 401));
-        }
+    const { fullName, username, password, confirmPassword, gender } = req.body;
 
-        const user = await User.findOne({username});
-
-        if(user) {
-            return next(new AppError("Username already exists", 400));
-        }
-
-        // Hashing Password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // generating profile pics
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
-
-        const newUser = await User.create({
-            fullName,
-            username,
-            password: hashedPassword,
-            gender,
-            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
-        });
-
-        res.status(201).json({
-            status: 'success',
-            data:{
-                newUser,
-            }
-        });
-
-    } catch (error) {
-        console.log("Error in signup controller", error.message);
-        res.status(500).json({error: "Internal Server Error"});
+    if (password !== confirmPassword) {
+        return next(new AppError("Passwords Do Not Match", 400));
     }
-}
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+        return next(new AppError("Username already exists", 400));
+    }
+
+    // Hashing Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // generating profile pics
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await User.create({
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+    });
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            newUser,
+        }
+    });
+
+
+});
 
 export const login = (req, res) => {
     console.log('Login user')
