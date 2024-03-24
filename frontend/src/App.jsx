7ce9a25deb6dacc8dc205eaf5a65c1630ae42469
Anushk Jain <anushk2201@gmail.com>
2024-03-24 23:43:01 +0530

    done: basic signup page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+// import SignUp from "./pages/signup/SignUp";
+// import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      <Home />
     </div>
   )
 }
