f16ee26a927a97345b99a50274a854768eaeddd0
Anushk Jain <anushk2201@gmail.com>
2024-03-25 23:57:01 +0530

    feat: search bar for users working.

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
+
 const SearchInput = () => {
-  return (
-    <div>
-      <form className="flex items-center gap-2 mt-2">
-        <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
+	const [search, setSearch] = useState("");
+	const {setSelectedConversation} = useConversation();
+	const {conversations} = useGetConversations();
 
-        <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-            <IoSearch />
-        </button>
-      </form>
-    </div>
-  )
-}
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if(!search) return;
 
-export default SearchInput
+		if(search.length < 3) {
+			return toast.error("Search term must be alteast 3 characters long");
+		}
+
+		const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+		if(conversation) {
+			setSelectedConversation(conversation);
+			setSearch('');
+		} else {
+			toast.error("No such user found!");
+		}
+	}
+
+	return (
+		<div>
+			<form className='flex items-center gap-2 mt-2' onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Search...'
+					className='input input-bordered rounded-full'
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+
+				<button
+					type='submit'
+					className='btn btn-circle bg-sky-500 text-white'
+				>
+					<IoSearch />
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default SearchInput;
