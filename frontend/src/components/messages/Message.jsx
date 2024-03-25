6aa454aabc143665af34fdef1d2acf8f560f64fd
Anushk Jain <anushk2201@gmail.com>
2024-03-25 23:40:18 +0530

    feat: time of the message sent is visible now and it automatically scrolls to the last message now.

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,10 +1,13 @@
 import {useAuthContext} from "../../context/AuthContext"
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 export const Message = ({message}) => {
 	const {authUser} = useAuthContext();
 	const {selectedConversation} = useConversation();
 	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+
 	const chatClassName = fromMe ? 'chat-end' : 'chat-start';
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
@@ -20,8 +23,8 @@ export const Message = ({message}) => {
 				</div>
 			</div>
 
-            <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
-            <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>12:42</div>
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+            <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>{formattedTime}</div>
 		</div>
 	);
 };
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,18 +1,26 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import { Message } from "./Message";
 
 export const Messages = () => {
 	const { messages, loading } = useGetMessages();
+	const lastMessageRef = useRef();
 
-	console.log("messages: ", messages);
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
 
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
 			{!loading &&
 				messages.length > 0 &&
 				messages.map((message) => (
-					<Message key={message._id} message={message} />
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
 				))}
 
 			{loading &&
