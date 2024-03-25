48333a6211a6d7b5e7c2898eba0f007af2e085f9
Anushk Jain <anushk2201@gmail.com>
2024-03-26 02:57:11 +0530

    done: changes for deployment.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 
-// now, we have changed the type into module, we 
+// now, we have changed the type into module, we have to import not require
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,11 +13,13 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-import  globalErrorMiddleware  from "./controllers/error.controller.js";
+import globalErrorMiddleware from "./controllers/error.controller.js";
 import { app, server } from "./socket/socket.js";
 
 
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -29,7 +32,14 @@ app.use(cookieParser()); //to parse the cookie
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
 app.use(globalErrorMiddleware);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
     connectToMongoDB();
